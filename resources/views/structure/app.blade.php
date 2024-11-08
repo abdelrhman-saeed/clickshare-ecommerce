8667460fad90b7a9c79e202690b8000c501d8da8
abdelrhman-saeed <abdelrhmansaeed001@gmail.com>
2024-11-08 23:00:09 +0200

    add navbar

--- resources/views/structure/app.blade.php
+++ resources/views/structure/app.blade.php
@@ -20,6 +20,37 @@
 </head>
 <body>
 
+    <nav class="navbar navbar-expand-lg navbar-light default-bg p-4">
+
+        <a class="navbar-brand fw-bold text-light" href="{{url('/')}}">Click Share</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+
+                <li class="nav-item active">
+                    <a class="nav-link text-light" href="{{url('/')}}">Home</a>
+                </li>
+
+                @auth
+
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="{{route('products.index')}}">Products</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="{{route('signout')}}">Logout</a>
+                    </li>
+
+                @endauth
+
+            </ul>
+        </div>
+
+    </nav>
     <!--  body section -->
     @yield('body')
     <!--  /body section -->
--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,14 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome');
 Route::view('signing', 'users.signing')->name('signing');
 
-Route::post('signin', [AuthController::class, 'signin'])->name('signin');
-Route::post('signup', [UserController::class, 'store'])->name('signup');
+Route::post('signin', [AuthController::class, 'signin'])
+        ->name('signin');
 
-Route::get('signout', [AuthController::class, 'signout']);
+Route::post('signup', [UserController::class, 'store'])
+        ->name('signup');
+
+Route::get('signout', [AuthController::class, 'signout'])
+        ->name('signout');
 
 Route::resource('products', ProductController::class)
         ->middleware(RedirectIfAuthenticated::class);
